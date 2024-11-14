dd992b0e45bc774bbc35b579d9549f6a09c24464
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2024-11-14 15:00:51 +0100

    <chore>:<building the main structure of the start page and connect to game page + general structure of the game page>

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,17 @@
+import { NgFor, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+  pickCardAnimation = false;
+takeCard(){
+  this.pickCardAnimation = true;
 
+}
 }
